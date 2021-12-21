76d9c25a56e478e6ef8caceeb236766ab8ecd86d
sebas <sebasgonra@gmail.com>
2021-12-21 12:20:47 -0400

    update

--- contract/mediator/src/lib.rs
+++ contract/mediator/src/lib.rs
@@ -1,14 +1,26 @@
-#[allow(dead_code)] 
-
+use near_sdk::{env, near_bindgen, AccountId, setup_alloc, Balance, PanicOnDefault, EpochHeight};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::json_types::{ValidAccountId};
 use near_sdk::serde::{Serialize};
-//use near_sdk::serde_json;
-use near_sdk::{env, near_bindgen, AccountId};
-use near_sdk::collections::{LookupMap, UnorderedMap};
+use near_sdk::collections::{LookupMap, UnorderedMap, LazyOption, Vector};
+use std::convert::TryFrom;
 
-#[global_allocator]
-static ALLOC: near_sdk::wee_alloc::WeeAlloc<'_> = near_sdk::wee_alloc::WeeAlloc::INIT;
+const YOCTO_NEAR: u128 = 1000000000000000000000000;
+const STORAGE_PRICE_PER_BYTE: Balance = 10_000_000_000_000_000_000;
+const MAX_JURIES: u8 = 20;
+
+pub(crate) fn string_to_valid_account_id(account_id: &String) -> ValidAccountId{
+    return ValidAccountId::try_from((*account_id).to_string()).unwrap();
+}
+
+pub(crate) fn unique_prefix(account_id: &AccountId) -> Vec<u8> {
+    let mut prefix = Vec::with_capacity(33);
+    prefix.push(b'o');
+    prefix.extend(env::sha256(account_id.as_bytes()));
+    prefix
+}
+
+setup_alloc!();
 
 pub type DisputeId = u128;
 pub type ServiceAmount = u64;
@@ -19,16 +31,7 @@ pub struct Vote {
     // Miembro del jurado que emite el voto
     account: AccountId,
     // Decisión tomada 
-    ruling: u8,
-}
-
-#[derive(Serialize, BorshDeserialize, BorshSerialize)]
-#[serde(crate = "near_sdk::serde")]
-pub struct VoteCounter {
-    // Votos realizados
-    votes: Vec<u8>,
-    // Contador de votos
-    votes_counter: u8
+    ruling: bool,
 }
 
 #[derive(Serialize, BorshDeserialize, BorshSerialize)]
@@ -44,41 +47,56 @@ pub enum DisputeStatus {
 pub struct Dispute {
     // Identificador para cada disputa
     id: DisputeId,
+    services_id: u64,
+
     // Cantidad de  miembros de jurado para la disputa
-    number_jurors: u8,
+    max_juries: u8,
+
     // Lista de miembros del jurado y sus respectivos services a retirar
     jury: LookupMap<AccountId, ServiceAmount>,
     dispute_status: DisputeStatus,
-    vote: Vote,
-    vote_counter: VoteCounter,
-    winner: u8,
-    //TODO agregar contador para el tiempo
+    votes: Vector<Vote>,
+    initial_epoch_height: EpochHeight, //time
+    
+    winner: LazyOption<AccountId>,
+    applicant: AccountId, // demandante
+    accused: AccountId, // acusado
 
+    applicant_proves: LazyOption<String>, // Un markdown con todas las pruebas
+    accusedt_proves: LazyOption<String> // Un markdown con todas las pruebas
 }
 
 #[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct Mediator {
-    admin: ValidAccountId,
+    // admin: ValidAccountId,
     disputes: UnorderedMap<DisputeId, Dispute>,
     disputes_counter: u128,
 }
 
-impl Default for Mediator {
-    fn default() -> Self {
-        env::panic(b"Mediator should be initialized before usage");
-    }
-}
-
 #[near_bindgen]
 impl Mediator {
+    #[init]
+    pub fn new() -> Self{
+        assert!(!env::state_exists(), "Contract already inicialized");
+        let this = Self {
+            disputes: UnorderedMap::new(b"d"),
+            disputes_counter: 0
+        };
 
-    #[private]
-    fn finished_assert(&self, dispute_id: &DisputeId) {
-        assert_eq!(*dispute_id, DisputeStatus::Finished, "The dispute must be finished");
+        return this;
     }
 
-    #[private]
-    fn admin_assert(&self, account_id: &AccountId) {
-        assert_eq!(*account_id, self.admin, "Must be admin_id how call its function");
+    pub fn new_dispute(services_id: u64, max_juries: u8, accused: ValidAccountId, proves: String) {
+        
     }
+    // #[private]
+    // fn finished_assert(&self, dispute_id: &DisputeId) {
+    //     assert_eq!(*dispute_id, DisputeStatus::Finished, "The dispute must be finished");
+    // }
+
+    // #[private]
+    // fn admin_assert(&self, account_id: &AccountId) {
+    //     assert_eq!(*account_id, self.admin, "Must be admin_id how call its function");
+    // }
 }
